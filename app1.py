324f0e0c2b07af84c16d6264f8d1ca6fe7ea7b04
EbrahimGhanbari <ghanbari@ualberta.ca>
2020-09-02 17:51:34 -0600

    fixed issue with lower case

--- app1.py
+++ app1.py
@@ -5,8 +5,11 @@ with open("data.json") as fileContent:
     content = json.load(fileContent)
 
 def translate(word):
-    return content[word]
+    lowerCasedWord = word.lower()
+    if lowerCasedWord in content:
+        return content[lowerCasedWord]
+    else:
+        return "The word does not exist! Please double check"
 
 word = input("Enter a word: ")
 print(translate(word))
-# print(content["rain"])
