1800833f61d8b9e8ab14b9b9def80cb9134a7b91
EbrahimGhanbari <ghanbari@ualberta.ca>
2020-09-02 19:47:45 -0600

    add suggesting word

--- app1.py
+++ app1.py
@@ -1,15 +1,29 @@
 import json
+from difflib import get_close_matches
+
 
 
 with open("data.json") as fileContent:
     content = json.load(fileContent)
 
+def wordSuggestion(word):
+    suggestedWord = get_close_matches(word, content.keys())[0]
+    confirmation = input(f"Unfortunately we could not find {word}! Did you mean {suggestedWord}? (Y/N) ")
+    if confirmation.lower() == "y":
+        return translate(suggestedWord)
+
+
 def translate(word):
     lowerCasedWord = word.lower()
     if lowerCasedWord in content:
         return content[lowerCasedWord]
-    else:
-        return "The word does not exist! Please double check"
+    elif len(get_close_matches(word, content.keys())) > 0:
+        suggestedWord = get_close_matches(word, content.keys())[0]
+        confirmation = input(f"Unfortunately we could not find {word}! Did you mean {suggestedWord}? (Y/N) ")
+        if confirmation.lower() == "y":
+            return translate(suggestedWord)
+
+    return "The word does not exist! Please double check"
 
 word = input("Enter a word: ")
 print(translate(word))
