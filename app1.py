507caffed5c69038de81526c1d7fc71c1137ddcf
EbrahimGhanbari <ghanbari@ualberta.ca>
2020-09-02 20:02:21 -0600

    refractore the code

--- app1.py
+++ app1.py
@@ -10,15 +10,18 @@ def translate(word):
     lowerCasedWord = word.lower()
     if lowerCasedWord in content:
         return content[lowerCasedWord]
+    elif word.title() in content: #if user entered "texas" this will check for "Texas" as well.
+        return content[word.title()]
     elif len(get_close_matches(word, content.keys())) > 0:
         suggestedWord = get_close_matches(word, content.keys())[0]
         confirmation = input(f"Unfortunately we could not find {word}! Did you mean {suggestedWord}? (Y/N) ")
         if confirmation.lower() == "y":
             return translate(suggestedWord)
 
-    return "The word does not exist! Please double check"
+    return ["The word does not exist! Please double check"]
 
 word = input("Enter a word: ")
+
 for item in translate(word):
     print(item)
 
